7c6581a51586d3b302db46eaa6781ed2331eebd3
Lautaro <lautarobertone13@gmail.com>
2021-11-28 00:07:49 -0300

    add post create task and configuration

--- back/database.py
+++ back/database.py
@@ -1,12 +1,25 @@
+import re
 from pony.orm import *
 
 db = Database()
 
 
 # id generated auto
-class Elements(db.Entity):
+class Task(db.Entity):
     name = Required(str)
 
 
 db.bind("sqlite", "files_db", create_db=True)
 db.generate_mapping(create_tables=True)
+
+
+# methods
+@db_session
+def AddTask(taskName: str):
+    try: 
+        newTask = Task(name=taskName)
+        newTask.flush()
+        return newTask.to_dict(["id","name"])
+    except:
+        return -1
+
--- back/main.py
+++ back/main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
+from database import *
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class JoinTask(BaseModel):
+    task: str
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.post("/createTask")
+async def createTask(input: JoinTask):
+    print(input.task)
+    result = AddTask(input.task)
+    if result == -1:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            details="Error in the create task",
+        )
+    print(result)
+    return result
+
+
+# @app.get("/")
+# def prueba():
+#     return 1
